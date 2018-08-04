d54eb6f6e1aa295d35e2c78c27c4148024201827
SandraCGarcia <sandra.gcanete@gmail.com>
2018-08-04 16:52:45 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,73 @@
+"use strict";
+
+//ejercicio 1
+
+// var nombre = document.querySelector('.name');
+// var usuario = 'sandra';
+// var usuario2 = 'nuria';
+// var boton = document.querySelector('.button');
+
+// function tuNombre (){
+// console.log(tuNombre);
+// if (usuario === nombre.value){
+//     alert ('Bienvenida ' + nombre.value);
+//     }else if (usuario2 === nombre.value){
+//     alert ('Bienvenida ' + nombre.value);
+// }else{
+//     alert (nombre.value + ' es un usuario no registrado');
+// }
+
+// }
+// boton.addEventListener ('click', tuNombre);
+
+//ejercicio 2 sin terminar
+
+// var number = prompt('Introduce un número');
+// var condicion1 = 0;
+
+// function numero(){
+//     console.log (numero);
+// if (number === condicion1) {
+//     alert('El número es 0');
+//   } else if (/* condicion 1 */) {
+//     alert('El número es negativo')
+//   } else if (/* condicion 1 */) {
+//     alert('El número es múltiplo de 2 y de 3')
+//   } else if (/* condicion 1 */) {
+//     alert('El número es mayor que 20 pero menor que 50')
+//   } else {
+// alert('el número no es 123123125');
+//   }
+// }
+
+//ejercicio 3
+
+// var ageDog = parseInt(prompt("¿Que edad tiene tu perro?"));
+// var ageHuman = 0;
+
+// if (ageDog === 1) {
+//   ageHuman = 15;
+//   alert("Tu perro tiene " + ageHuman + " años humanos");
+// } else if (ageDog === 2) {
+//   ageHuman = 24;
+//   alert("Tu perro tiene " + ageHuman + " años humanos");
+// } else if (ageDog >= 3) {
+//   ageHuman = 24 + 5 * (ageDog - 2);
+//   alert("Tu perro tiene " + ageHuman + " años humanos");
+// }
+
+//ejercicio 4
+
+// for (var i = 0; i < 10; i++) {
+//     console.log('Voy por la vuelta ' + i);
+//   }
+
+//ejercicio 5
+
+
+
+var result = 0;
+
+for (var i = 0; i < 10; i++) {
+     console.log('Voy por la vuelta ' + result + 2);
+  }
